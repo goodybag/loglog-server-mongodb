d2bd0ab5e97b51fb97f51dbf317eab709dd561fc
John Fawcett <jrf0110@gmail.com>
2014-09-03 12:06:06 -0500

    implement proper check, close db clients

--- index.js
+++ index.js
@@ -2,25 +2,46 @@ var MongoClient = require('mongodb').MongoClient;
 
 module.exports = function( options ){
   return {
-    query: function( query, callback ){
+    check: function( callback ){
+      var query = {
+        where: this.lastResults.length ? { _id: { $gt: this.lastResults[0]._id } } : {}
+      , options: {
+          sort: [ [ '_id', -1 ] ]
+        , limit: 20
+        }
+      };
+
+      this.query( query, function( error, results ){
+        if ( error ) return callback( error );
+        this.lastResults = results;
+        return callback( null, results );
+      }.bind( this ) );
+    }
+
+  , lastResults: []
+
+  , query: function( query, callback ){
       query.where   = query.where || {};
       query.options = query.options || {};
 
       MongoClient.connect( options.connection, function( error, db ){
         if ( error ) return callback( error );
 
-        var cursor = db.collection( options.collecition ).find( query.where );
+        var cursor = db.collection( options.collection ).find( query.where );
 
         // Options
         [
-          'sort', 'skip'
+          'sort', 'skip', 'limit'
         ].forEach( function( option ){
           if ( option in query.options ){
             cursor[ option ]( query.options[ option ] );
           }
         });
 
-        cursor.toArray( callback );
+        cursor.toArray( function( error, results ){
+          db.close();
+          callback( error, results );
+        });
       });
     }
   }
